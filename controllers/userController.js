f09c4277411d1d2985dcd82c1d03974cbd84cf73
Mavrk1604 <mavrk1604@gmail.com>
2024-07-16 21:42:09 -0500

    Autenticacion de usuario y login agregados

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,6 @@
 const User = require('./../models/User')
 const bcrypt = require('bcrypt')
+const { generateToken } = require('./../middlewares/jwtGenerate')
 
 const createUser = async (req, res) => {
   const { email, password } = req.body
@@ -29,6 +30,36 @@ const createUser = async (req, res) => {
   }
 }
 
+const userLogIn = async (req, res) => {
+  const { email, password } = req.body
+  try {
+    const dbUser = await User.findOne({ email })
+    if (!dbUser) return res.status(400).json({
+      ok: false,
+      msg: `User doesn't exist.`
+    })
+    const validatePassword = bcrypt.compareSync(password, dbUser.password)
+    if (!validatePassword) return res.status(400).json({
+      ok: false,
+      msg: `Password incorrect.`
+    })
+    const token = await generateToken(dbUser._id, dbUser.email)
+
+    return res.status(200).json({
+      ok: true,
+      msg: `${dbUser.email} Bienvenido a Frodge!`,
+      token: token
+    })
+  } catch(error) {
+    console.log(error)
+    return res.status(500).json({
+      ok: false,
+      msg: 'Please contact support.'
+    })
+  }
+}
+
 module.exports = {
-  createUser
+  createUser,
+  userLogIn
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const { createUser } = require('./../controllers/userController')
+const { createUser,userLogIn } = require('./../controllers/userController')
 const users = require('./../middlewares/validationBody')
 const validateFields = require('./../middlewares/validationResult')
 
 router.post('/register', users, validateFields, createUser)
+router.post('/login', userLogIn)
 
 module.exports = router
