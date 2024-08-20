1b9e7ad436722a2fddbcb7f4211572259741ca18
Mavrk1604 <mavrk1604@gmail.com>
2024-08-20 18:52:06 -0500

    proyecto angluar eliminado del backend frodge

--- prueba/src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { EventsComponent } from './components/events/events.component';
-import { MainComponent } from './components/main/main.component';
-
-export const routes: Routes = [
-  { path: '', title: 'Inicio', component: MainComponent },
-  { path: 'events', title: 'Eventos', component: EventsComponent }
-];
