b6b85ab21c137497e2f54095baa1f2cde6827ea4
Mavrk1604 <mavrk1604@gmail.com>
2024-08-22 18:36:18 -0500

    schema de productos modificado

--- models/Product.js
+++ models/Product.js
@@ -8,40 +8,17 @@ const productsSchema = Schema({
   },
   type: {
     type: String,
-    required: true
   },
   description: {
     type: String,
     required: true,
-    unique: true
   },
-  information: {
+  conservation: {
     type: String,
-    required: false,
-  },
-  fridge: {
-    type: Boolean,
-    required: true
-  },
-  freeze: {
-    type: Boolean,
-    required: true
-  },
-  oven: {
-    type: Boolean,
-    required: true
-  },
-  stove: {
-    type: Boolean,
-    required: true
-  },
-  price: {
-    type: Number,
     required: true,
   },
   vegetarian: {
     type: Boolean,
-    required: true
   }
 
 })
