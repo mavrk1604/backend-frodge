aecc3457d60f19f8603c3ecf9bc228692f4d3346
Mavrk1604 <mavrk1604@gmail.com>
2024-09-13 21:10:50 -0500

    version prod de angular agregada y lineas anadidas a index.js

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const dotenv = require('dotenv')
 const api = require('./routes/api.routes')
 const cors = require('cors')
+const path = require('path')
 
 dotenv.config();
 const port = process.env.PORT
@@ -12,6 +13,10 @@ databaseConnect()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cors())
+app.use('/', express.static(__dirname + '/dist/frontend-frodge/browser'))
+app.get('/*', (req,res, next) => {
+  res.sendFile(path.resolve(__dirname + '/dist/frontend-frodge/browser/index.html'))
+})
 app.use('/', api)
 
 app.listen(8080, () => {
